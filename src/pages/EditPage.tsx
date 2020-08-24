25293872555190b7640f1e6fc844663e11672588
bboti199 <b.boti199@yahoo.com>
2020-08-24 13:05:10 +0300

    finishing touches

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/rootReducer';
-import { makeStyles, Theme, createStyles, Grid, Card, CardContent, Typography } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, Grid, Card, CardContent, Typography, Button } from '@material-ui/core';
 import { EditReservationForm } from '../components/elements';
 
 interface EditPageRouterProps {
@@ -30,6 +30,10 @@ export const EditPage: React.FC<EditPageProps> = ({ match }: EditPageProps) => {
 
   return (
     <Grid container direction='column' justify='center' alignItems='center'>
+      <Typography>
+        Warning: Refreshing the page deletes all existing data due to the fact that it is generated on the fly and is
+        stored in memory
+      </Typography>
       <Card className={classes.root}>
         <CardContent>
           {reservation ? (
@@ -40,9 +44,14 @@ export const EditPage: React.FC<EditPageProps> = ({ match }: EditPageProps) => {
               <EditReservationForm reservation={reservation} />
             </React.Fragment>
           ) : (
-            <Typography gutterBottom variant='h4' className={classes.title}>
-              We did not find your reservation
-            </Typography>
+            <Grid container direction='column' justify='center' alignItems='center'>
+              <Typography gutterBottom variant='h4' className={classes.title}>
+                We did not find your reservation
+              </Typography>
+              <Button component={Link} to='/' variant='contained' color='default'>
+                Go Back
+              </Button>
+            </Grid>
           )}
         </CardContent>
       </Card>
